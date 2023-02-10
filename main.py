cbd6e0522a040afb665d8d0d2cfea709575c553e
Codepro2011 <96655543+Codepro2011@users.noreply.github.com>
2023-02-10 21:02:16 +0100

    Oppdater main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 bitbot.led_rainbow()
 bitbot.led_rotate()
-bitbot.rotatems(BBRobotDirection.LEFT, 60, 1200)
+bitbot.rotatems(BBRobotDirection.LEFT, 60, 1500)
 bitbot.goms(BBDirection.FORWARD, 60, 600)
 bitbot.rotatems(BBRobotDirection.LEFT, 60, 1200)
 bitbot.goms(BBDirection.FORWARD, 60, 1000)
-bitbot.rotatems(BBRobotDirection.LEFT, 60, 300)
+bitbot.rotatems(BBRobotDirection.LEFT, 60, 250)
 bitbot.goms(BBDirection.FORWARD, 60, 1000)
-bitbot.rotatems(BBRobotDirection.LEFT, 60, 300)
+bitbot.rotatems(BBRobotDirection.LEFT, 60, 250)
 bitbot.goms(BBDirection.FORWARD, 60, 1000)
-bitbot.rotatems(BBRobotDirection.LEFT, 60, 300)
+bitbot.rotatems(BBRobotDirection.LEFT, 60, 250)
 bitbot.goms(BBDirection.FORWARD, 60, 1000)
-bitbot.rotatems(BBRobotDirection.LEFT, 60, 300)
+bitbot.rotatems(BBRobotDirection.LEFT, 60, 250)
+bitbot.goms(BBDirection.FORWARD, 60, 1400)
 
 def on_forever():
     pass
